565fa0090d7e3ecb610a24a4ab70083f6ce98948
JenniferFariasRodrigues <jennifer.farias.rodrigues@gmail.com>
2025-08-30 02:10:00 -0300

    refactor(api): reorganize packages backend - domain, dto, repository

--- backend/src/main/java/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-
-import domain.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {}
--- backend/src/main/java/repository/TaskRepository.java
+++
@@ -1,5 +0,0 @@
-package repository;
-
-import domain.Task;
-import org.springframework.data.jpa.repository.*;
-public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {}
